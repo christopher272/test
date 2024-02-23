c2af1a972692b23e15ed329d7bca98a3678252bb
christopher272 <66278915+christopher272@users.noreply.github.com>
2024-02-23 11:47:02 -0700

    Delete unit 1 directory

--- unit 1/loops&arrays/loops.js
+++
@@ -1,7 +0,0 @@
-var officeItems = ["stapler", "monitor", "computer", "desk", "lamp", "computer", "lamp", "stapler", "computer",  "computer"];
-var officeData = ["0"];
-for(var i = 0; i < officeItems.length; i++){
-    if(officeItems[i] === "computer"){officeData ++;}
-   
- }
- console.log(officeData);
